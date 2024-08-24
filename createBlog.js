a67a07ac73bbad0931efeed688f7f9a1b220ca8d
Joseph___ <118039462+josephethiopia@users.noreply.github.com>
2024-08-24 12:41:36 +0300

    update

---
+++ createBlog.js
@@ -0,0 +1,37 @@
+document
+  .getElementById("blog-form")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {};
+    for (let [key, value] of formData.entries()) {
+      if (key === "tags") {
+        data[key] = value.split(", ");
+      } else {
+        data[key] = value;
+      }
+    }
+    try {
+      const response = await fetch(
+        "https://deployment-demo-backend-aitk.onrender.com/api/posts",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      if (response.status === 201) {
+        alert("Blog Post Created Successfully!");
+        setTimeout(() => {
+          window.location.href = "home.html";
+        }, 2000);
+      } else {
+        const res = await response.json();
+        alert(res.error);
+      }
+    } catch (e) {
+      alert(e.message);
+    }
+  });
---
+++ scripts.js
@@ -0,0 +1,79 @@
+let signup = document.querySelector(".signup-btn");
+let login = document.querySelector(".login-btn");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+  slider.classList.add("moveslider");
+  formSection.classList.add("form-section-move");
+});
+
+login.addEventListener("click", () => {
+  slider.classList.remove("moveslider");
+  formSection.classList.remove("form-section-move");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login_form");
+  const signupForm = document.getElementById("signup_form");
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = loginForm.querySelector(".name").value;
+    const password = loginForm.querySelector(".password").value;
+
+    try {
+      const response = await fetch(
+        "https://deployment-demo-backend-aitk.onrender.com/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        window.location.href = "home.html";
+        console.log("Token:", data.token);
+      } else {
+        alert(data.error);
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  });
+
+  signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = signupForm.querySelector(".name").value;
+    const email = signupForm.querySelector(".email").value;
+    const password = signupForm.querySelector(".password").value;
+
+    try {
+      const response = await fetch(
+        "https://deployment-demo-backend-aitk.onrender.com/auth/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, email, password }),
+        }
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        alert("Signup successful!");
+      } else {
+        alert(data.error);
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  });
+});
